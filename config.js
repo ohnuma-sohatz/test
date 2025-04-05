d42773030d84ddfc5dbbad97e6998e75c064a89a
ohnuma-sohatz <masaru.ohnuma@sohatz.co.jp>
2025-04-05 19:26:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.difyChatbotConfig = {
   token: '70Oc3Pbq9DT31NWw',
   dynamicScript: true,
-  autoRestoreOnNavigation: false // ← これでオン・オフ切り替え可能に
+  autoRestoreOnNavigation: true // ← これでオン・オフ切り替え可能に
 };
